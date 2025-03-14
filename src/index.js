3e8fcabe25b852382e30c8075c09db6ea5610bc5
HasanAlizadeh <texnovideo181@gmail.com>
2025-03-14 16:44:48 +0400

    wtf that

--- src/index.js
+++ src/index.js
@@ -4,21 +4,31 @@ require('dotenv').config();
 const fs = require('fs');
 const DiscordBot = require('./client/DiscordBot');
 
+console.log("Creating DiscordBot client...");
 const client = new DiscordBot();
 
 // Load event handlers
+console.log("Loading event handlers...");
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
+  console.log(`Loading event: ${file}`);
   const event = require(`./events/${file}`);
   if (event.once) {
-    client.once(event.name, (...args) => event.execute(...args));
+    client.once(event.name, (...args) => {
+      console.log(`Event triggered: ${event.name}`);
+      event.execute(...args);
+    });
   } else {
-    client.on(event.name, (...args) => event.execute(...args));
+    client.on(event.name, (...args) => {
+      console.log(`Event triggered: ${event.name}`);
+      event.execute(...args);
+    });
   }
 }
 
 module.exports = client;
 
+console.log("Connecting to Discord...");
 client.connect();
 
 process.on('unhandledRejection', error => {
